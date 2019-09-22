44e0ece28cfe46441820bf7d4881ea48ebad0604
Mirsho <mirshozz@gmail.com>
2019-09-22 21:28:02 +0100

    4th commit - Code changed to english

--- js/file.js
+++ js/file.js
@@ -1,30 +1,30 @@
-var resultado = document.getElementById('resultado');
+var result = document.getElementById('resultado');
 
-function tablaMultiplicar(){   //!función mal escrita: falta la 'i' en 'Multiplicar'
+function multiplicationTable(){   //! misspelled function: 'i' missing in 'multplication'
   var n = document.getElementById('dato').value;
   var m;
-  var tabla = '';
+  var table = '';
   if (isNaN(n) || n === '') {
-    tabla = '¡¡ Error !! >>> Dato mal escrito.';
-    resultado.style.backgroundColor = '#f00';
+    table = '¡¡ Error !! >>> El dato introducido es incorrecto.';
+    result.style.backgroundColor = '#f00';
   } else {
-    tabla = '<table>';    //? ¿Debería poner borde a la tabla?
-    resultado.style.backgroundColor = 'inherit';
+    table = '<table>';    //? should I put border to the table?
+    result.style.backgroundColor = 'inherit';
     n = parseInt(n);
     m = n+9;
-    tabla += '<tr><td></td>';
+    table += '<tr><td></td>';
     for (var k=1; k<=10;k++) {
-      tabla += '<td>' + k + '</td>';
+      table += '<td>' + k + '</td>';
     }
-    tabla += '</tr>';
-    for (var i=n;i<=m;i++) {    //*En este for cambiamos la condición i<=n por i<= m 
-      tabla += '<tr><td>'+ i +'</td>';
+    table += '</tr>';
+    for (var i=n;i<=m;i++) {    //* In this for loop, we change condition from 1<=n to i<=m 
+      table += '<tr><td>'+ i +'</td>';
       for (var j=1; j<=10;j++){
-          tabla += '<td data-n="'+ i +'x'+ j +'=' + (i*j) +'">' + (i*j) + '</td>';
+          table += '<td data-n="'+ i +'x'+ j +'=' + (i*j) +'">' + (i*j) + '</td>';
         }
-      tabla += '</tr>';
+      table += '</tr>';
       }
-    tabla += '</tabla>';  
+    table += '</table>';  
   }
-  resultado.innerHTML = tabla;
+  result.innerHTML = table;
 }
